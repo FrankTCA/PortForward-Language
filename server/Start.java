b24a0c98087019be467b64926f24cd12eee6e373
FrankTCA <frank@steamedveggie.com>
2016-10-05 08:16:20 -0500

    Start.java edit 1

--- server/Start.java
+++ server/Start.java
@@ -1,1 +1,16 @@
 package server;
+
+public class Start {
+  public static void main(String[] args) {
+    if (args.length == 3) {
+      Server server = new Server(args[0], args[1], args[2]);
+      while (true) {
+        int response = server.waitAndRespondToClientRequest();
+        System.out.println()
+      }
+    } else {
+      Date date = new Date();
+      System.out.println("[" + date.toString() + "] FATAL: Invalid arguments. Please use arguments <root directory> <script directory> <database>");
+    }
+  }
+}
