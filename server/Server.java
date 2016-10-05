51583aa5d70fb2f3919496353f4509bb1a6fff5f
FrankTCA <frank@steamedveggie.com>
2016-10-05 08:24:49 -0500

    Server.java edit 1

--- server/Server.java
+++ server/Server.java
@@ -1,1 +1,11 @@
 package server;
+
+public class Server {
+  File root;
+  File scripts;
+  File database;
+  ServerSocket s;
+  
+  public Server(String rootDir, String scriptDir, String dataDir, int port) {
+    root = new File(rootDir);
+    scripts = new File(scriptDir);
