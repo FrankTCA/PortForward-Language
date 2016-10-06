c9885928fe6e883bb65b24c88d815c7d72d193d4
FrankTCA <frank@steamedveggie.com>
2016-10-06 11:19:32 -0500

    RequestMethod.java edit 2

--- http/RequestMethod.java
+++ http/RequestMethod.java
@@ -2,6 +2,26 @@ package http;
 
 public class RequestMethod {
   public Request getMethod(String address) {
-    
+    Gson json = new Gson();
+    String databasePath = ServerSession.getDatabase();
+    File usercache = new File(databasePath + "/usercache.json");
+    BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(usercache)));
+    String line;
+    String jsonToRead = "";
+    while ((line = reader.readLine()) != null) {
+      jsonToRead = jsonToRead + line;
+    }
+    JsonObject fromJson = json.fromJson(jsonToRead, JsonObject.class);
+    String[] addresses = fromJson.get("addresses");
+    String method;
+    String[] methods;
+    for (int x = addresses.length; x > 0; x--) {
+      if (addresses[x].equals(address)) {
+        methods = fromJson.get("methods");
+        method = methods[x];
+        Method returnStatement = new Method(method);
+        return returnStatement;
+      }
+    }
   }
 }
