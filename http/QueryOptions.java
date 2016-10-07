98f2381fde69c23b1d16d49a00ae9fd1b04bb19e
FrankTCA <frank@steamedveggie.com>
2016-10-07 08:52:44 -0500

    QueryOptions.java edit 1

--- http/QueryOptions.java
+++ http/QueryOptions.java
@@ -1,5 +1,12 @@
 package http;
 
 public class QueryOptions {
-
+  String address;
+  public QueryOptions(String inetAddress) {
+    address = inetAddress;
+  }
+  
+  public String get(String name) {
+    
+  }
 }
